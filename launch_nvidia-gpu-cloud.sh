0e0938e7ae3359035b55acfcdcd1a34a0fe4a083
JeeWon Jung <jungjee@JeeWonui-MacBook-Pro.local>
2020-09-22 10:22:32 +0900

    line change

--- launch_nvidia-gpu-cloud.sh
+++ launch_nvidia-gpu-cloud.sh
@@ -9,7 +9,8 @@ save_dir=[DIR_TO_SAVE_AND_LOG_EXPERIMENTS]
 DB_dir=[DIR_OF_YOUR_DOWNLOADED_DBS]
 docker_image=nvcr.io/nvidia/pytorch:19.10-py3 #when conducting experiments for DcaseNet, we 1. launched this official image then 2. manually updated torch version to "1.4.0".
 
-sudo nvidia-docker run -it --rm --ipc=host --shm-size 20G -v ${local_dir}:/code \
+sudo nvidia-docker run -it --rm --ipc=host --shm-size 20G \
+  -v ${local_dir}:/code \
   -v ${DB_dir}:/DB \
   -v ${save_dir}:/exp torch_140 
 
